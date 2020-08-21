7645604edb3a55f47b8733dd58e7bfcf3002d152
Adam Spice <adwasere@gmail.com>
2020-08-21 12:41:18 +1200

    Added character and basic movement

---
+++ Dave.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+const SPEED = 230
+const GRAVITY = 35
+const JUMPFORCE = -950
+
+func _physics_process(delta):
+	if Input.is_action_pressed("right"):
+		velocity.x = SPEED
+	if Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+	
+	velocity.y = velocity.y + GRAVITY
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMPFORCE
+		
+		
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	velocity.x = lerp(velocity.x, 0, 0.3)
+
+	
